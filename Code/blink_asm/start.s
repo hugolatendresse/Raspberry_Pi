4e03c0bb47efd53cf250e4ecf825ef755601a416
Hugo Latendresse <hugolatendresse@gmail.com>
2024-04-28 19:03:41 -0700

    Blink.py as is

--- Code/blink_asm/start.s
+++ Code/blink_asm/start.s
@@ -1,6 +1,9 @@
-/* This is a comment */
 .global main /* Main is our entry point and must be global */
 
+.equ VIR_LEG, 0x7e2000e8
+.equ VIR, 0x47e2000e8
+.equ PHY, 0x4C02000e8
+
 main:
-	mov r0, #3  /* Put a 2 inside the register r0 */
+	mov r0, #0x10000000  /* Put a value inside the register r0 */
 	bx lr /* Return from main */
